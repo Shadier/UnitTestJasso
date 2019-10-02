1008009c32c73bdf41dcbfea42f4eb358fae4f40
Raúl Moreno <raulmoreno899@gmail.com>
2019-10-02 16:42:08 -0500

    Three Unit test to add user method

---
+++ UtilidadesRepositorio.Tests/UserTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UtilidadesRepositorio.Tests
+{
+    [TestClass]
+    public class UsersTests
+    {
+        [TestMethod]
+        public void addNewUser_CorrectData()
+        {
+            //Arrange o Planteamiento
+            var user = new User();
+            user.employeeID = 12314;
+            user.FirstName = "Raúl";
+            user.LastName = "Moreno";
+
+            //Act o Prueba
+            var actual = Assignments.addUser(user);
+
+            //Assert o Afirmación
+            Assert.AreEqual(true, actual);
+        }
+
+        [TestMethod]
+        public void addNewUser_IncorrectName()
+        {
+            //Arrange o Planteamiento
+            var user = new User();
+            user.employeeID = 12314;
+            user.FirstName = "Alex4";
+            user.LastName = "Moreno";
+
+            //Act o Prueba
+            var actual = Assignments.addUser(user);
+
+            //Assert o Afirmación
+            Assert.AreEqual(false, actual);
+        }
+
+        [TestMethod]
+        public void addNewUser_IncorrectLengthOfId()
+        {
+            //Arrange o Planteamiento
+            var user = new User();
+            user.employeeID = 114;
+            user.FirstName = "Alex";
+            user.LastName = "Moreno";
+
+            //Act o Prueba
+            var actual = Assignments.addUser(user);
+
+            //Assert o Afirmación
+            Assert.AreEqual(false, actual);
+        }
+
+    }
+}
---
+++ UtilidadesRepositorio/Users.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilidadesRepositorio
+{
+    public class User
+    {
+        public string FirstName;
+        public string LastName;
+        public int employeeID;
+    }
+    public class Assignments
+    {
+        public static bool addUser(User user)
+        {
+            bool isText = true;
+            for (int i = 0; i < user.FirstName.Length; i++)
+            {
+                if (Char.IsNumber(user.FirstName, i))
+                    isText = false;
+            }
+
+            for (int i = 0; i < user.LastName.Length; i++)
+            {
+                if (Char.IsNumber(user.LastName, i))
+                    isText = false;
+            }
+            bool wrongLength = (user.employeeID < 1000)? true : false;
+            return isText && !wrongLength;
+        }
+
+        public static int Division(int dividendo, int divisor)
+        {
+            if (divisor == 0)
+                throw new DivideByZeroException("¡No se puede dividir entre CERO!");
+            
+            return dividendo/divisor;
+        }
+    }
+}
