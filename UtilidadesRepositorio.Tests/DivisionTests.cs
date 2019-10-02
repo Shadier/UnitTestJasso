280d8ca1abdc6246973e89dd1cec8209a2996ea4
Raúl Moreno <raulmoreno899@gmail.com>
2019-10-02 16:00:08 -0500

    First commit

---
+++ UtilidadesRepositorio.Tests/DivisionTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UtilidadesRepositorio.Tests
+{
+    [TestClass]
+    public class DivisionTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void Division_DivideEntreCero()
+        {
+            //Arrange o Planteamiento
+            const int dividendo = 8;
+            const int divisor = 0;
+
+            //Act o Prueba
+            var actual = Matematicas.Division(dividendo, divisor);
+
+            //Assert => Excepción
+        }
+    }
+}
